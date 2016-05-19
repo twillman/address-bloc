dfb753e46745f4f754a363528c345b68ac0e4711
Tara Willman <tara.willman@gmail.com>
2016-05-19 07:13:04 -0700

    Checkpoint 22 searching: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -28,6 +28,23 @@ class AddressBook
     end
   end
 
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+    return nil
+  end
+
   def remove_entry(name, phone_number, email)
     index = 0
     entries.each do |entry|
@@ -36,6 +53,6 @@ class AddressBook
       end
       index +=1
     end
-  entries.delete_at(index)
+    entries.delete_at(index)
   end
 end
